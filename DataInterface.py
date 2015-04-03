71df85d0f7ae29b53e1a6a52d51e5e854e5f1683
Neftali Dominguez <ndominguez@g.hmc.edu>
2015-04-03 14:10:59 -0700

    updated db so we can save file

--- DataInterface.py
+++ DataInterface.py
@@ -12,7 +12,7 @@ class DataInterface:
     important to maintain the integrity of the student data categories and the headerList instance variable to prevent
     errors in which one attempts to modify the attributes of a category which does not exist. """
 
-    def __init__(self, fileloc=None, year=None, semester=None):
+    def __init__(self, fileloc="", year="", semester=""):
         """ Creates the database. If a file location (fileloc), the function open up a previously saved database.
         If no file location is passed, the function will use newDB to create a new database. """
         
@@ -33,7 +33,7 @@ class DataInterface:
         file.write(filename)
 
     
-    def newDB(self, year=None, semester=None):
+    def newDB(self, year="", semester=""):
         """ Returns the root of a new ElementTree named "Gradebook" with the three subelements "Students", "Groups",
         and "Dates" for the three categories of data that could be stored in the database. Also stores the year
         and semester attributes for the Gradebook if those are passed. """
@@ -60,7 +60,7 @@ class DataInterface:
         # finds the first non-flagged, non-dropped student and sets the list of their children to catlist.
         stulist = self.data.find("Students").getchildren()
         x = 0
-        while(stulist[x].find("Flag").attrib["info"] == True or stulist[x].find("In Class").attrib["info"] == False):
+        while(stulist[x].find("Flag").attrib["info"] == "Yes" or stulist[x].find("In Class").attrib["info"] == "No"):
             x += 1
         catlist = stulist[x].getchildren()
 
@@ -77,15 +77,15 @@ class DataInterface:
         Student has all current column categories but without any values. """
         
         students = self.data.find("Students")  # finds the Students data category.
-        student = SubElement(students, name)  # adds a new subelement with the student's name as a tag.
-
+        student = SubElement(students, "Name") # adds a new subelement with the student's name as a tag.
+        student.attrib["info"] = name
         # adds all default student data categories.
         SubElement(student, "ID")
         SubElement(student, "Units")
         SubElement(student, "Number of Absences")
         SubElement(student, "Number of Excused")
-        SubElement(student, "In Class").attrib["info"] = True
-        SubElement(student, "Flag").attrib["info"] = False
+        SubElement(student, "In Class").attrib["info"] = "Yes"
+        SubElement(student, "Flag").attrib["info"] = "No"
 
         # adds all additional data categories.
         for x in range(0, len(self.headerList)):
@@ -107,7 +107,7 @@ class DataInterface:
         v2list = []
 
         for x in range(0, len(vlist)):
-            if(vlist[x].find("Flag").attrib["info"] == False or vlist[x].find("In Class").attrib["info"] == True):
+            if(vlist[x].find("Flag").attrib["info"] == "No" or vlist[x].find("In Class").attrib["info"] == "Yes"):
                    nlist.append(vlist[x].tag)
         nlist.sort()
 
@@ -121,7 +121,7 @@ class DataInterface:
         """ Sets the InClass attribute to indicate that the student has dropped. """
         
         student = self.findStudent(name)
-        student.find("In Class").attrib["info"] = 0
+        student.find("In Class").attrib["info"] = "No"
 
     
     def stuMod(self, name, header, value):
@@ -137,7 +137,7 @@ class DataInterface:
         return student.find(header).attrib["info"]
 
     
-    def stuAdd(self, header, value=None):
+    def stuAdd(self, header, value=""):
         """ Adds a category with the tag given by the header to all students and adds this category to the list of
         categories which will be given to students added in the future. If a default is given in the value input
         this will be given to all students. This function will add duplicate categories, which will cause errors.
@@ -148,7 +148,7 @@ class DataInterface:
         clist = students.getchildren()
 
         for x in range(0, len(clist)):
-            if (value != None):
+            if (value != ""):
                 SubElement(clist[x], header).attrib["info"] = value
             else:
                 SubElement(clist[x], header)
@@ -187,7 +187,7 @@ class DataInterface:
         slist = self.stuSort(slist)
         vlist = []
 
-        if(header not in self.headerList): return None
+        if(header not in self.headerList): return ""
 
         for x in range(0, len(slist)):
             vlist.append(slist[x].find(header).attrib["info"])
