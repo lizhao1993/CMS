9adc874f43751ec7218435341ecca4b21546bd0f
lizhao1993 <lizhao1993@gmail.com>
2015-04-03 13:30:14 -0800

    EXE

--- example.py
+++ example.py
@@ -1,32 +1,37 @@
 #this is the current version 03/31/15
-
-
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+import loadworkbook
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QFileDialog, QAction,
+                             QTextEdit)
 from CMS1 import Ui_MainWindow
-from PyQt5.QtWidgets import (QMainWindow, QTextEdit, 
-    QAction, QFileDialog, QApplication)
-#from pyexcel as pe import json
+
 
 app = QApplication(sys.argv)
 window = QMainWindow()
 ui = Ui_MainWindow()
 ui.setupUi(window)
-#need to load old table
 
-    
 def pushButton_Clicked(self):
-    print ("hello")
+    print("hello")
     fname = QFileDialog.getOpenFileName()
-    #with xlrd.open_workbook(fname) as f: # import as excel
-    sheet= pe.load(fname)
-    #print json.dumps(sheet.to_array())
-    ui.tableView.setText(data) 
-    
-        
-ui.pushButton.clicked.connect(pushButton_Clicked)
+    filename = (fname[0])
+    students = loadworkbook.getStudentsFromWorkbook(filename)
+    print(students)
 
+model = QStandardItemModel(2,3)
+model.setHorizontalHeaderItem(0, QStandardItem("Name"))
+model.setHorizontalHeaderItem(1, QStandardItem("Email"))
+model.setHorizontalHeaderItem(2, QStandardItem("Units"))
+
+firstRow = QStandardItem("ColumnValue")
+model.setItem(0,0,firstRow)
+
+ui.tableView.setModel(model)
+ui.pushButton.clicked.connect(pushButton_Clicked)
 
 window.show()
 sys.exit(app.exec_())
+
 
